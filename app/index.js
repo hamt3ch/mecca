6154243c878d24671e72c6c4bd7934734562f0dc
hamt3ch <hmiles23@ufl.edu>
2016-09-24 21:01:55 -0700

    refactor index.js and moved passport to its own module

--- app/index.js
+++ app/index.js
@@ -24,70 +24,9 @@ mongoose.connect(url, function(err) {
   console.log("Connected successfully to server w/ mongoose");
 });
 
-//TESTING
-var schema = require('./user').model;
-var User = mongoose.model('User', schema);
-
-// Configuring Passport
-var LocalStrategy = require('passport-local').Strategy;
- var passport = require('passport');
- var expressSession = require('express-session');
- app.use(expressSession({secret: 'mySecretKey'}));
- app.use(passport.initialize());
- app.use(passport.session());
-
-passport.serializeUser(function(user, done) {
-  done(null, user._id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
-passport.use('local', new LocalStrategy({
-    usernameField: 'username',
-    passwordField: 'password',
-    passReqToCallback: true
-  },
-  function(req, username, password, done) {
-    User.findOne({ 'name' : username },
-    function(err, user) {
-      // In case of any error, return using the done method
-      if (err)
-        return done(err);
-      // Username does not exist, log error & redirect back
-      if (!user){
-        console.log('User Not Found with username '+username);
-        return done(null, false, "user not found");
-      }
-      // User exists but wrong password, log the error
-      if (user.password != password){
-        console.log('Invalid Password');
-        return done(null, false, "Invalid password");
-      }
-      // User and password both match, return user from
-      // done method which will be treated like success
-      return done(null, user);
-    }
-  );
-  }
-));
-
-app.post('/login',
-  passport.authenticate('local'),
-  function(req, res) {
-    // If this function gets called, authentication was successful.
-    // `req.user` contains the authenticated user.
-    //  return access/bearer token w/ user info
-    res.json(req.user);
-  });
-
-app.get('/error', function(req, res) {
-  console.log("didnt work");
-  res.json({message:"error"});
-});
+// Configuring passport
+auth = require('./auth')(app);
 
 
+// Export app
 module.exports = app;
