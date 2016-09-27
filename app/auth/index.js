4acac7abf4a3b2c5825332142da0e6660ef84cd0
hamt3ch <hmiles23@ufl.edu>
2016-09-26 21:58:58 -0700

    fixed conflicts

--- app/auth/index.js
+++ app/auth/index.js
@@ -13,6 +13,64 @@ module.exports = function(app){
   // list of all the routes available for passport
   app.post('/login',
     passport.authenticate('local'),
+
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+    clientID: "318092088551180",
+    clientSecret: "f5fa7a25f0f042c7f16f78fef2afc04a",
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    //console.log(accessToken);
+    //console.log(refreshToken);
+    //console.log(profile);
+    // done(null, null);
+    User.findOne({ 'id' : profile.id},
+    function(err, user) {
+      if (err) {
+        console.log(err);
+      } if (!user) {
+        // no user found create one
+        var newUser = new User({
+          appId: 'test_app',
+          provider: 'facebook',
+          data: {
+              'id': profile.id,
+              'profile': profile,
+              'accessToken': accessToken,
+              'refreshToken': refreshToken
+          }
+        });
+        return done(null, newUser);
+      } if (user) {
+        // found user
+        return done(null, user);
+      }
+    });
+  }
+));
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    //  return access/bearer token w/ user info
+    res.json(req.user);
+  });
+
+app.post('/success',
+  passport.authenticate('local'),
     function(req, res) {
       // If this function gets called, authentication was successful.
       // `req.user` contains the authenticated user.
