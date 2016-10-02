f9015254f33b6961a70cfeea44e7aa660c0cdb0e
hamt3ch <hmiles23@ufl.edu>
2016-10-01 20:23:07 -0700

    working react view

--- app/index.js
+++ app/index.js
@@ -27,6 +27,11 @@ mongoose.connect(url, function(err) {
 // Configuring passport
 var auth = require('./auth')(app);
 
+//ReactJS views
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.get('/', require('./routes').index);
 
 // Export app
 module.exports = app;
